34c333210efcf44fb249a5a87713bc98c7951110
Brock Sauvage <bsauvage14@gmail.com>
2026-06-25 17:27:07 -0500

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from setup import setup_config
+from pipeline.simulate_elections import simulate_elections
+
 def main():
     print("Hello from kansas-city-alternative-election-analysis!")
+    config = setup_config()
+    simulate_elections(config)
+
 
 
 if __name__ == "__main__":
